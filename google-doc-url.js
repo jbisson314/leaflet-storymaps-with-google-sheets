95a6d965fa229bd62b48753f0dfd1523b7ab9428
jbisson314 <37672660+jbisson314@users.noreply.github.com>
2018-03-29 15:58:09 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1acJ1tyiSlzHzERw4Oe92uHxWUwHji26EfsWsLF40ehc';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1acJ1tyiSlzHzERw4Oe92uHxWUwHji26EfsWsLF40ehc/';
